f1b3120b241250b1e41e022729fa73524ec155db
Jan Němec <nemec.jan123@gmail.com>
2022-07-06 22:44:55 +0200

    Preparation for external map resources

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
+// JS Requests: https://stackoverflow.com/questions/247483/http-get-request-in-javascript
+class ApiComms {
+    static GetRequest(url: string) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url, false);
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    }
+
+    static GetRequestAsync(url: string, callback: Function) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+        }
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+    }
+}
+
 interface MapEntity {
     GetMapEntity(): any;
 }
@@ -6,33 +26,35 @@ class MapMarker implements MapEntity {
     public point: L.LatLng;
     public popupMsg: string;
 
-    public constructor(point: L.LatLng, popupMsg?: string) {
+    public constructor(point: L.LatLng, popupMsg: string) {
         this.point = point;
         this.popupMsg = popupMsg;
     }
 
     public GetMapEntity(): L.Marker {
         var marker = L.marker([this.point.lat, this.point.lng]);
-        if (this.popupMsg !== "undefined")
-            marker.bindPopup(this.popupMsg);
+        marker.bindPopup(this.popupMsg);
         return marker;
     }
 }
 
 class MapRoad implements MapEntity {
     private points: L.LatLng[];
+    private popupMsg: string;
     private color: string;
     private weight: number;
     private opacity: number;
     private smoothFactor: number;
 
     public constructor(points: L.LatLng[],
+                popupMsg: string,
                 color: string = "red",
                 weight: number = 5,
                 opacity: number = 0.5,
                 smoothFactor: number = 1
                 ) {
         this.points = points;
+        this.popupMsg = popupMsg;
         this.color = color;
         this.weight = weight;
         this.opacity = opacity;
@@ -40,12 +62,14 @@ class MapRoad implements MapEntity {
     }
 
     public GetMapEntity(): L.Polyline {
-        return new L.Polyline(this.points, {
+        var polyline = new L.Polyline(this.points, {
             color: this.color,
             weight: this.weight,
             opacity: this.opacity,
             smoothFactor: this.smoothFactor
         });
+        polyline.bindPopup(this.popupMsg);
+        return polyline;
     }
 }
 
@@ -89,10 +113,25 @@ class MapLayer {
     }
 }
 
+class GhostMapLayer {
+    public layerName: string;
+    private layerLink: string;
+
+    public constructor(name: string, link: string) {
+        this.layerName = name;
+        this.layerLink = link;
+    }
+
+    public Download(): MapLayer {
+        return new MapLayer([], [], this.layerName);
+    }
+}
+
 // TS Singleton: https://stackoverflow.com/questions/30174078/how-to-define-singleton-in-typescript
 class App {
     private mapWindow: MapWindow;
     private mapLayers: MapLayer[] = [];
+    private ghostMapLayers: GhostMapLayer[] = [];
     private static _instance: App;
 
     private constructor(){};
@@ -110,6 +149,16 @@ class App {
         this.RenderLayerList();
     }
 
+    public AddGhostMapLayer(ghostMapLayer: GhostMapLayer) {
+        this.ghostMapLayers.push(ghostMapLayer);
+        this.RenderLayerList();
+    }
+
+    private RemoveGhostMapLayer(index: number) {
+        this.ghostMapLayers.splice(index, 1);
+        this.RenderLayerList();
+    }
+
     private RenderLayerList() {
         const layersList = document.getElementById("layersList");
         layersList.innerHTML = "";
@@ -120,6 +169,22 @@ class App {
             listItem.innerHTML = l.layerName;
             listItem.setAttribute("class", "list-group-item");
             listItem.setAttribute("href", `javascript:App.Instance.ActivateMapLayer(${i})`);
+            layersList.appendChild(listItem);
+        }
+
+        // Non-existent (ghost) layers (Note: external sources)
+        for (let i = 0; i < this.ghostMapLayers.length; i++) {
+            const g = this.ghostMapLayers[i];
+            var listItem = document.createElement("a");
+            listItem.innerHTML = g.layerName;
+            listItem.setAttribute("class", "list-group-item list-group-item-danger d-flex justify-content-between align-items-center");
+            listItem.setAttribute("href", `javascript:App.Instance.DownloadGhostLayer(${i})`);
+            
+            var badge = document.createElement("span");
+            badge.innerHTML = "Ke stažení";
+            badge.setAttribute("class", "badge bg-primary rounded-pill");
+            
+            listItem.appendChild(badge);
             layersList.appendChild(listItem);
         }
     }
@@ -138,6 +203,20 @@ class App {
         const mapLayerNewState = !mapLayer.GetAndToggleActiveState();
         this.SetActiveInLayerList(index, mapLayerNewState);
         this.mapWindow.RenderMapLayer(this.mapLayers[index], mapLayerNewState);
+    }
+
+    private SetDownloadingInLayerList(index: number) {
+        const layers = document.getElementById("layersList").children;
+        index += this.mapLayers.length;
+        layers[index].setAttribute("class", "list-group-item list-group-item-warning d-flex justify-content-between align-items-center");
+        layers[index].children[0].innerHTML = "Stahování...";
+    }
+
+    public DownloadGhostLayer(index: number) {
+        let ghostLayer = this.ghostMapLayers[index];
+        this.SetDownloadingInLayerList(index);
+        // this.mapLayers.push(ghostLayer.Download());
+        // this.RemoveGhostMapLayer(index);
     }
 }
 
@@ -194,12 +273,20 @@ let myRoads = new MapLayer([], [
         new L.LatLng(49.861, 15.512),
         new L.LatLng(49.86, 15.513),
         new L.LatLng(49.86, 15.514)
-    ]),
+    ], "This is a road popup #1!"),
     new MapRoad([
         new L.LatLng(49.859, 15.511),
         new L.LatLng(49.859, 15.512),
         new L.LatLng(49.858, 15.513),
         new L.LatLng(49.859, 15.514)
-    ])
+    ], "This is a road popup #2!")
 ], "Moje cesty #1");
+
 app.AddMapLayer(myRoads);
+
+let apiLayer = new GhostMapLayer("Moje cesty z externího API", "http://api.com/endpoint");
+let apiLayer2 = new GhostMapLayer("Moje cesty z externího API 2", "http://api.com/endpoint");
+app.AddGhostMapLayer(apiLayer);
+app.AddGhostMapLayer(apiLayer2);
+
+ApiComms.GetRequestAsync("https://catfact.ninja/fact", console.log);
